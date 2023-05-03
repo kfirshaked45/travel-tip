2f9fca3d0d75f8190cb1b10a42193d9066a80a0e
Kfir Shaked <kfirshaked150@gmail.com>
2023-05-03 16:54:01 +0300

    new

---
+++ js/services/loc.service.js
@@ -0,0 +1,72 @@
+import { utilService } from './util.service.js';
+import { storageService } from './async-storage.service.js';
+
+const LOCATION_KEY = 'location-db';
+
+_createLocations();
+
+export const locService = {
+  query,
+  get,
+  remove,
+  save,
+  getEmptyLocation,
+  createDemoLocations,
+  getLocs,
+};
+
+function query() {
+  return storageService.query(LOCATION_KEY).then((locations) => {
+    return locations;
+  });
+}
+function getLocs() {
+  return storageService.query(LOCATION_KEY).then((locations) => {
+    return locations;
+  });
+}
+
+function get(locationId) {
+  return storageService.get(LOCATION_KEY, locationId);
+}
+
+function remove(locationId) {
+  return storageService.remove(LOCATION_KEY, locationId);
+}
+
+function save(location) {
+  if (location.id) {
+    return storageService.put(LOCATION_KEY, location);
+  } else {
+    return storageService.post(LOCATION_KEY, location);
+  }
+}
+
+function getEmptyLocation(name = '', lat = 0, lng = 0) {
+  return {
+    id: '',
+    name,
+    lat,
+    lng,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+}
+
+function createDemoLocations() {
+  const locations = [
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+    { name: 'LostCity', lat: 35.1001, lng: -106.6706 },
+    { name: 'City of the Dead', lat: 29.9823, lng: 31.1342 },
+    { name: 'Abandoned Island', lat: 1.3542, lng: 103.9915 },
+  ];
+  return utilService.saveToStorage(LOCATION_KEY, locations);
+}
+
+function _createLocations() {
+  let locations = utilService.loadFromStorage(LOCATION_KEY);
+  if (!locations || !locations.length) {
+    createDemoLocations();
+  }
+}
---
+++ js/services/util.service.js
@@ -0,0 +1,41 @@
+export const utilService = {
+  getCurrentDateTime,
+  makeId,
+  getRandomIntInclusive,
+  loadFromStorage,
+  saveToStorage,
+};
+
+function getCurrentDateTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const minute = String(now.getMinutes()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hour}:${minute}`;
+}
+
+function makeId(length = 5) {
+  var txt = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return txt;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function saveToStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromStorage(key) {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : undefined;
+}
